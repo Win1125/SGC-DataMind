0c3c10a1bca8ff96f47a066adc892da36f3369fc
Win1125 <edwineladio73@gmail.com>
2024-04-05 19:43:16 -0500

    Delete Notifications

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,7 +2,7 @@ import NotificationModel from "../models/notification.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import { NextFunction, Request, Response } from "express";
-import { create } from "domain";
+import cron from "node-cron";
 
 // Get all notifications -- only admin
 export const getNotifications = CatchAsyncError( async (req:Request, res:Response, next: NextFunction) => {
@@ -20,7 +20,7 @@ export const getNotifications = CatchAsyncError( async (req:Request, res:Respons
     }
 });
 
-// Update notification status
+// Update notification status -- only admin
 export const updateNotificationStatus = CatchAsyncError( async (req:Request, res:Response, next: NextFunction) => {
     try {
 
@@ -49,3 +49,10 @@ export const updateNotificationStatus = CatchAsyncError( async (req:Request, res
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+// Delete notifications -- only admin
+cron.schedule("0 0 0 * * *", async () => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    await NotificationModel.deleteMany({status:"read", createdAt: {$lt: thirtyDaysAgo}});
+    console.info("Deleted Notifications");
+})
